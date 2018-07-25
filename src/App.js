7c0fda69f02e19460f197c03446ad1514ee8122d
AhmadNoor02 <41566242+AhmadNoor02@users.noreply.github.com>
2018-07-25 10:50:13 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Applications from './Applications';
+import Application from './Application';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Applications />
+        <Application />
       </div>
     );
   }
